d1c852bb19f5b8c2418ee434d3dc95fe8d196468
millerh1 <millerh1@livemail.uthscsa.edu>
2022-03-16 16:40:03 -0500

    Prepped for collaboration

--- misc/scratch.R
+++ misc/scratch.R
@@ -1,4 +1,4 @@
-## Scratch work
+## Scratch work showing how to install reticulate and load autoPLIER
 
 # Install from github
 reticulate::py_install("git+https://github.com/dmontemayor/autoplier.git@devel", pip = T)
