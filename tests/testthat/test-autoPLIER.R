aa4e9bfafc505ea7ea0ca3d4ec4133826121ad10
Erica Lam <eri.lam@outlook.com>
2022-03-23 15:32:25 -0500

    Added wrapper for autoPLIER class

--- tests/testthat/test-autoPLIER.R
+++ tests/testthat/test-autoPLIER.R
@@ -1,2 +1,12 @@
 # Test goes here
 # Test should verify that autoPLIER class object can be successfully created
+
+library(testthat)
+test_that(
+    "Can instantiate autoPLIER as an S3 class",
+    {
+        ap <- autoPLIER(n_components=200)
+        expect_is(ap, class = "autoplier.model.autoPLIER")
+    }
+)
+
