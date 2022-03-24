23cf2ffa9736eeaae2d7803af0acad7b36d5cf07
Erica Lam <eri.lam@outlook.com>
2022-03-23 19:49:10 -0500

    update tests

--- tests/testthat/test-autoPLIER.R
+++ tests/testthat/test-autoPLIER.R
@@ -1,11 +1,37 @@
-# Test goes here
-# Test should verify that autoPLIER class object can be successfully created
+### Tests the autoPLIER object and its methods ###
 
+# Set the conda environment
+Sys.setenv(RETICULATE_PYTHON = "~/miniconda3/envs/test-autoplier/bin/python")
+reticulate::use_condaenv("test-autoplier", required = TRUE)
+
+# Libraries
+library(autoplieR)
 library(testthat)
+
+# Get test data from package's extdata folder
+testxfl <- system.file("extdata", "test_X.csv.xz", package = "autoplieR")
+pathwayfl <- system.file("extdata", "test_pathways.csv.xz", package = "autoplieR")
+x_train <- read.csv(testxfl, row.names = 1)
+pathways <- read.csv(pathwayfl, row.names = 1)
+
+# Test class
 test_that(
     "Can instantiate autoPLIER as an S3 class",
     {
         ap <- autoPLIER(n_components=200)
+        expect_is(ap, class = "autoplier.model.autoPLIER")
+    }
+)
+
+# Test fit method
+test_that(
+    "Test fit",
+    {
+        ap <- autoPLIER(n_components=100)
+        ap <- autoPLIER.fit(
+            ap, x_train=x_train, pathways=pathways,
+            maxepoch=100L, verbose=0
+        )
         expect_is(ap, class = "autoplier.model.autoPLIER")
     }
 )
