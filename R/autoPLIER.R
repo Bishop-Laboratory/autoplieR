1427f177283b10037b9b1fb636eb648ec13ea80b
Erica Lam <eri.lam@outlook.com>
2022-03-30 14:52:10 -0500

    Add documentation for func params

--- R/autoPLIER.R
+++ R/autoPLIER.R
@@ -1,4 +1,10 @@
 #' AutoPLIER class
+#' 
+#' @param n_components Number of latent variables/latent dimensions.
+#' @param regval Regularization value.
+#' @param learning_rate Proportion that weights are updated during training.
+#' @param scaler Scaler to transform data.
+#' 
 #' @export
 autoPLIER <- function(...) {
     check_ap()
@@ -9,12 +15,53 @@ autoPLIER <- function(...) {
 
 #' AutoPLIER Fit Method
 #'
-#' @param x_train x_train does this thing
+#' @param x_train Training dataset used to train the model.
+#' @param pathways Pathways dataset.
+#' @param callbacks List of callbacks to apply during training.
+#' @param batch_size Fixed batch size.
+#' @param maxepoch Max number of epochs.
+#' @param verbose Verbosity mode. 0 = silent, 1 = progress bar, 
+#' 2 = one line per epoch.
+#' @param valfrac Fraction of the training dataset used as validation data.
+#' 
 #' @export
 autoPLIER.fit <- function(ap, ...) {
     ap$fit(...)  # Object is modified in memory
     ap  # Returns modified object
 }
+
+
+#' AutoPLIER Transform Method
+#' 
+#' @param x_predict Dataset to be transformed into the latent space. 
+#' @param pathways Pathways dataset.
+#' 
+#' @export
+
+    
+#' 
+#' AutoPLIER Fit-Transform Method
+#' 
+#' @param x_train Training dataset used to train the model.
+#' @param pathways Pathways dataset.
+#' @param callbacks List of callbacks to apply during training.
+#' @param batch_size Fixed batch size.
+#' @param maxepoch Max number of epochs.
+#' @param verbose Verbosity mode. 0 = silent, 1 = progress bar, 
+#' 2 = one line per epoch.
+#' @param valfrac Fraction of the training dataset used as validation data.
+
+
+
+#' AutoPLIER Get Top Pathways Method
+#' 
+#' @param LVs List of decomposed latent variables.
+#' @param n_pathways Number of pathways to retrieve.
+#' 
+#' AutoPlIER Get Top Pathways LVs Method
+#' 
+#' @param pathway List of pathways.
+#' @param n_LVs Number of latent variables to retrieve.
 
 # TODO: Put in the rest of the methods from autoPLIER object
 # TODO: Document all arguments, description, and examples
