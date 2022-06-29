6d1ae1dd93923e769c22312c98e853093f6feff5
Erica Lam <eri.lam@outlook.com>
2022-06-29 07:27:44 -0500

    Finishing touches to vignette, wrapper, fxn doc

--- R/autoPLIER.R
+++ R/autoPLIER.R
@@ -2,15 +2,18 @@
 #'
 #' Calls the constructor autoPLIER to create a model.
 #'
-#' @param n_components Number of latent variables/latent dimensions.
-#' @param regval Regularization value. Default: `1.20E-7`.
-#' @param learning_rate Proportion that weights are updated during training.
-#' Default: `0.0002`.
-#' @param scaler Scaler to transform data.
+#' @param ... Additional arguments for autoPLIER. See *details*.
 #' @return A Python object called `autoplier.model.autoPLIER` that cannot be
-#' viewed.
+#'   viewed.
+#' @details
+#' ## Additional arguments
+#' * **n_components** - Number of latent variables/latent dimensions.
+#' * **regval** - Regularization value. Default: `1.20E-7`.
+#' * **learning_rate** - Proportion that weights are updated during training.
+#'   Default: `0.0002`.
+#' * **scaler** - Scaler to transform data.
 #'
-#' @example
+#' @examples
 #' mod <- autoPLIER(n_components = 50)
 #'
 #' @export
@@ -28,25 +31,31 @@ autoPLIER <- function(...) {
 #' model.
 #'
 #' @param ap AutoPLIER object.
-#' @param x_train Training dataset used to train the model.
-#' @param pathways Pathways dataset.
-#' @param callbacks List of callbacks to apply during training.
-#' @param batch_size Fixed batch size. Default: `NULL`.
-#' @param maxepoch Max number of epochs. Default: `2000`.
-#' @param verbose Verbosity mode. 0 = silent, 1 = progress bar,
-#' 2 = one line per epoch. Default: `2`.
-#' @param valfrac Fraction of the training dataset used as validation data.
-#' Default: `0.3`.
+#' @param ... Additional arguments for autoPLIER.fit. See *details*.
 #' @return A Python object called `autoplier.model.autoPLIER` that cannot be
-#' viewed.
+#'   viewed.
+#' @details
+#' ## Additional arguments
+#' * **x_train** - Training dataset used to train the model.
+#' * **pathways** - Pathways dataset.
+#' * **callbacks** - List of callbacks to apply during training.
+#' * **batch_size** - Fixed batch size. Default: `NULL`.
+#' * **maxepoch** - Max number of epochs. Default: `2000`.
+#' * **verbose** - Verbosity mode. 0 = silent, 1 = progress bar,
+#'   2 = one line per epoch. Default: `2`.
+#' * **valfrac** - Fraction of the training dataset used as validation data.
+#'   Default: `0.3`.
 #'
-#' @example
+#' @examples
 #' # Example datasets
-#' xtrain <- read_csv(system.file("extdata", "GSE157103_icu_tpm.csv.xz", package = "autoplieR")
-#' pwy <- read_csv(system.file("extdata", "pathways.csv.xz", package = "autoplieR")
+#' xtrain <- read.csv(system.file("extdata", "GSE157103_icu_tpm.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#' pwy <- read.csv(system.file("extdata", "pathways.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
 #'
 #' # Fit the model
-#' mod <- autoPLIER.fit(mod, x_train = xtrain, pathways = pwy, verbose = 0)
+#' mod <- autoPLIER(n_components = 50)
+#' mod <- autoPLIER.fit(mod, x_train = xtrain, pathways = pwy, maxepoch=100L, verbose = 0)
 #'
 #' @export
 autoPLIER.fit <- function(ap, ...) {
@@ -61,12 +70,26 @@ autoPLIER.fit <- function(ap, ...) {
 #' Applies the fitted calculation to the created autoPLIER model.
 #'
 #' @param ap AutoPLIER object.
-#' @param x_predict Dataset to be transformed into the latent space.
-#' @param pathways Pathways dataset.
+#' @param ... Additional arguments for autoPLIER.transform. See *details*.
 #' @return A dataframe consisting of a number of LV columns as specified by the
-#' `n_components` in `autoPLIER()`.
+#'   `n_components` in `autoPLIER()`.
+#' @details
+#' ## Additional arguments
+#' * **x_predict** - Dataset to be transformed into the latent space.
+#' * **pathways** - Pathways dataset.
 #'
-#' @example
+#' @examples
+#' # Example datasets
+#' xtrain <- read.csv(system.file("extdata", "GSE157103_icu_tpm.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#' pwy <- read.csv(system.file("extdata", "pathways.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#'
+#' # Fit the model
+#' mod <- autoPLIER(n_components = 50)
+#' mod <- autoPLIER.fit(mod, x_train = xtrain, pathways = pwy, maxepoch=100L, verbose = 0)
+#'
+#' # Transform
 #' df_ap <- autoPLIER.transform(mod, x_predict = xtrain, pathways = pwy)
 #'
 #' @export
@@ -81,23 +104,35 @@ autoPLIER.transform <- function(ap, ...) {
 #' Simultaneously fit the data and transform the autoPLIER model.
 #'
 #' @param ap AutoPLIER object.
-#' @param x_train Training dataset used to train the model.
-#' @param pathways Pathways dataset.
-#' @param callbacks List of callbacks to apply during training.
-#' @param batch_size Fixed batch size. Default: `NULL`.
-#' @param maxepoch Max number of epochs. Default: `2000`.
-#' @param verbose Verbosity mode. 0 = silent, 1 = progress bar,
-#' 2 = one line per epoch. Default: `2`.
-#' @param valfrac Fraction of the training dataset used as validation data.
-#' Default: `0.3`.
+#' @param ... Additional arguments for autoPLIER.fit_transform. See *details*.
 #' @return A dataframe consisting of a number of LV columns as specified by the
-#' `n_components` in `autoPLIER()`.
+#'   `n_components` in `autoPLIER()`.
+#' @details
+#' ## Additional arguments
+#' * **x_train** - Training dataset used to train the model.
+#' * **pathways** - Pathways dataset.
+#' * **callbacks** - List of callbacks to apply during training.
+#' * **batch_size** - Fixed batch size. Default: `NULL`.
+#' * **maxepoch** - Max number of epochs. Default: `2000`.
+#' * **verbose** - Verbosity mode. 0 = silent, 1 = progress bar,
+#'   2 = one line per epoch. Default: `2`.
+#' * **valfrac** - Fraction of the training dataset used as validation data.
+#'   Default: `0.3`.
 #'
-#' @example
-#' df_ap <- autoPLIER.fit_transform(mod, x_train = xtrain, pathways = pwy, verbose = 0)
+#' @examples
+#' # Example datasets
+#' xtrain <- read.csv(system.file("extdata", "GSE157103_icu_tpm.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#' pwy <- read.csv(system.file("extdata", "pathways.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#'
+#' # Fit and transform
+#' mod <- autoPLIER(n_components = 50)
+#' df_ap <- autoPLIER.fit_transform(mod, x_train = xtrain, pathways = pwy, maxepoch=100L,
+#'  verbose = 0)
 #'
 #' @export
-autoPLIER.fit_transform <- function(ap, mod, ...) {
+autoPLIER.fit_transform <- function(ap, ...) {
     ap$fit_transform(...)
 
 }
@@ -109,32 +144,45 @@ autoPLIER.fit_transform <- function(ap, mod, ...) {
 #' Retrieve pathways most related to a list of LVs with coefficients.
 #'
 #' @param ap AutoPLIER object.
-#' @param LVs List of decomposed latent variables.
+#' @param LVs List of decomposed latent variables in an array.
 #' @param n_pathways Number of pathways to retrieve.
 #' @return A list containing sub-lists for each listed LV.
 #'
-#' @example
-#' autoPLIER.get_top_pathways(mod, LVs = top_LVs, n_pathways = 10)
+#' @examples
+#' # Example datasets
+#' xtrain <- read.csv(system.file("extdata", "GSE157103_icu_tpm.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#' pwy <- read.csv(system.file("extdata", "pathways.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#'
+#' # Fit and transform
+#' mod <- autoPLIER(n_components = 50)
+#' autoPLIER.fit_transform(mod, x_train = xtrain, pathways = pwy, maxepoch=100L, verbose=0)
+#'
+#' # Get top pathways
+#' autoPLIER.get_top_pathways(mod, array(c("LV_38", "LV_30")), n_pathways = 2)
 #'
 #' @export
 #' @importFrom dplyr %>% mutate filter rename group_by arrange
 #' @importFrom dplyr desc slice_max group_split group_keys
+#' @importFrom rlang .data
+#' @importFrom stats setNames
 #' @importFrom tibble rownames_to_column
 #' @importFrom tidyr pivot_longer
 autoPLIER.get_top_pathways <- function(ap, LVs, n_pathways) {
     ap$components_decomposition_ %>%
         rownames_to_column(var = "pathway") %>%
-        pivot_longer(cols = -pathway) %>%
+        pivot_longer(cols = -.data$pathway) %>%
         mutate(
             name = gsub(
-                name, pattern = "^([0-9]+)[ ]*$", replacement = "LV_\\1"
+                .data$name, pattern = "^([0-9]+)[ ]*$", replacement = "LV_\\1"
             )
         ) %>%
-        filter(name %in% {{ LVs }}) %>%
-        rename(LV=name) %>%
-        group_by(LV) %>%
-        arrange(LV, desc(value)) %>%
-        slice_max(order_by = value, n = n_pathways) %>%
+        filter(.data$name %in% {{ LVs }}) %>%
+        rename(LV=.data$name) %>%
+        group_by(.data$LV) %>%
+        arrange(.data$LV, desc(.data$value)) %>%
+        slice_max(order_by = .data$value, n = n_pathways) %>%
         {setNames(group_split(.), nm=group_keys(.)[[1]])} %>%
         as.list()
 }
@@ -146,18 +194,28 @@ autoPLIER.get_top_pathways <- function(ap, LVs, n_pathways) {
 #' Retrieve LVs associated with a named pathway.
 #'
 #' @param ap AutoPLIER object.
-#' @param pathway List of pathways.
-#' @param n_LVs Number of latent variables to retrieve.
+#' @param ... Additional arguments for autoPLIER.get_top_pathway_LVs. See *details*.
 #' @return Numeric object with the chosen pathway and its corresponding LVs.
+#' @details
+#' ## Additional arguments
+#' * **pathway** - List of pathways.
+#' * **n_LVs** - Number of latent variables to retrieve.
 #'
-#' @example
-#' autoPLIER.get_top_pathway_LVs(mod, pathway="BIOCARTA_VDR_PATHWAY", n_LVs=2L)
+#' @examples
+#' # Example datasets
+#' xtrain <- read.csv(system.file("extdata", "GSE157103_icu_tpm.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#' pwy <- read.csv(system.file("extdata", "pathways.csv.xz", package = "autoplieR"),
+#'  row.names = 1)
+#'
+#' # Fit and transform
+#' mod <- autoPLIER(n_components = 50)
+#' autoPLIER.fit_transform(mod, x_train = xtrain, pathways = pwy, maxepoch=100L, verbose=0)
+#'
+#' # Get LVs for a pathway
+#' mod <- autoPLIER.get_top_pathway_LVs(mod, pathway="BIOCARTA_VDR_PATHWAY", n_LVs=2L)
 #'
 #' @export
 autoPLIER.get_top_pathway_LVs <- function(ap, ...) {
     ap$get_top_pathway_LVs(...)
 }
-
-# TODO: Put in the rest of the methods from autoPLIER object
-# TODO: Document all arguments, description, and examples
-# TODO: Testthat fixture for unit testing
